e7a95fb759f684cea9d844fdfd28b9f651f7fbd9
ajayjoseaji <ajayjose350@gmail.com>
2024-02-14 17:20:54 +0530

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
-import { Layout } from "antd";
+"use client";
+
+import { Button, Layout } from "antd";
+import Link from "next/link";
+import { useAuth } from "./components/AuthContext";
 import { DashboardLayout } from "./components/DashboardLayout";
 
 export default function Home() {
+  const auth = useAuth();
+  if (!auth) {
+    return;
+  }
+  const { token } = auth;
   return (
     <Layout className="h-full" color="#455560">
       <DashboardLayout>
-        <h2>Landing Page</h2>
+        <div>
+          <h1 className="text-bold text-lg">Welcome to the Landing Page</h1>
+          {token ? (
+            <Link href="/dashboard">
+              <Button type="link">Go to Dashboard</Button>
+            </Link>
+          ) : (
+            <Link href="/login">
+              <Button type="link">Login</Button>
+            </Link>
+          )}
+        </div>
       </DashboardLayout>
     </Layout>
   );
